9d329a9b5a1646793d35db11abdc1f428a3ea82d
George Suveti <george.suveti@irian.ro>
2019-02-19 20:54:23 +0200

    add puppeteer

--- routes/html-to-pdf.js
+++ routes/html-to-pdf.js
@@ -1,9 +1,38 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
+const puppeteer = require('puppeteer')
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('html-to-pdf');
-});
+router.get('/', function (req, res, next) {
+  (async () => {
+    const {url} = req.query
+    console.log(url)
+    const buffer = await generatePDF(url)
+    res.send(buffer)
+  })()
+})
 
-module.exports = router;
+async function generatePDF (url) {
+  const browser = await puppeteer.launch({headless: true}) // Puppeteer can only generate pdf in headless mode.
+  const page = await browser.newPage()
+  await page.goto(url)
+  const pdfConfig = {
+    path: 'url.pdf', // Saves pdf to disk.
+    format: 'A4',
+    printBackground: true,
+    margin: { // Word's default A4 margins
+      top: '2.54cm',
+      bottom: '2.54cm',
+      left: '2.54cm',
+      right: '2.54cm'
+    }
+  }
+  await page.emulateMedia('screen')
+  const pdf = await page.pdf(pdfConfig) // Return the pdf buffer. Useful for saving the file not to disk.
+
+  await browser.close()
+
+  return pdf
+}
+
+module.exports = router
