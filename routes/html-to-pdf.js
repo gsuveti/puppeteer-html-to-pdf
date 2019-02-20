2790ba46ba0e326b0b1bdbc4a0ab8e4c7ff8fe6e
George Suveti <george.suveti@irian.ro>
2019-02-20 20:22:05 +0200

    generate pdf from html content

--- routes/html-to-pdf.js
+++ routes/html-to-pdf.js
@@ -2,20 +2,31 @@ var express = require('express')
 var router = express.Router()
 const puppeteer = require('puppeteer')
 
-/* GET users listing. */
 router.get('/', function (req, res, next) {
   (async () => {
     const {url} = req.query
-    console.log(url)
-    const buffer = await generatePDF(url)
+    const buffer = await generatePdfFromUrl(url)
+    res.contentType('application/pdf')
     res.send(buffer)
   })()
 })
 
-async function generatePDF (url) {
+router.post('/', function (req, res, next) {
+  (async () => {
+    const content = req.body
+    const {url} = req.query
+    const buffer = await generatePdfFromUrlAndContent(url, content)
+    res.contentType('application/pdf')
+    res.send(buffer)
+  })()
+})
+
+async function generatePdfFromUrl (url) {
   const browser = await puppeteer.launch({headless: true}) // Puppeteer can only generate pdf in headless mode.
   const page = await browser.newPage()
-  await page.goto(url)
+
+  await page.goto(url, {waitUntil: 'networkidle0'})
+
   const pdfConfig = {
     path: 'url.pdf', // Saves pdf to disk.
     format: 'A4',
@@ -31,7 +42,37 @@ async function generatePDF (url) {
   const pdf = await page.pdf(pdfConfig) // Return the pdf buffer. Useful for saving the file not to disk.
 
   await browser.close()
+  return pdf
+}
 
+async function generatePdfFromUrlAndContent (url, content) {
+  const browser = await puppeteer.launch({headless: true}) // Puppeteer can only generate pdf in headless mode.
+  const page = await browser.newPage()
+  if (url) {
+    await page.goto(url, {waitUntil: 'networkidle0'})
+  } else {
+    await page.goto(`data:text/html,<!DOCTYPE html><html><body></body></html>`, {waitUntil: 'networkidle0'})
+  }
+
+  await page.evaluate((content) => {
+    document.body.innerHTML = content
+  }, content)
+
+  const pdfConfig = {
+    path: 'url.pdf', // Saves pdf to disk.
+    format: 'A4',
+    printBackground: true,
+    margin: { // Word's default A4 margins
+      top: '2.54cm',
+      bottom: '2.54cm',
+      left: '2.54cm',
+      right: '2.54cm'
+    }
+  }
+  await page.emulateMedia('screen')
+  const pdf = await page.pdf(pdfConfig) // Return the pdf buffer. Useful for saving the file not to disk.
+
+  await browser.close()
   return pdf
 }
 
